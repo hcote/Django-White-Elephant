facb41494a2277ae5fada5f1ec8ad7150148b8ce
Hunter Cote <hcote12@gmail.com>
2018-11-06 19:59:11 -0800

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/huntercote/Desktop/code/apps/python/surprise-me/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
