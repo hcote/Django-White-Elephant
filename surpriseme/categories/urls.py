f4c3519025f9cf7c20392ede1ef562809ecc19f0
Hunter Cote <hcote12@gmail.com>
2018-11-22 21:21:12 -0500

    authentication for front end users is complete (not admins)

--- surpriseme/categories/urls.py
+++ surpriseme/categories/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details/<int:id>/', views.details, name='details'),    
+    path('details/<int:id>/', views.details, name='details'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
