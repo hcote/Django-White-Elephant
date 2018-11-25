4386595f879d420ad2aaf01d8a6876e359e56e0d
Hunter Cote <hcote12@gmail.com>
2018-11-25 13:26:32 -0500

    resolved issue caused by linking db_credentials module

--- surpriseme/settings.py
+++ surpriseme/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .db_credentials import db_name, db_password, db_user
+# from .db_credentials import db_name, db_password, db_user
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'surpriseme.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': db_name,
-        'USER': db_user,
-        'PASSWORD': db_password,
+        'NAME': 'surpriseme',
+        'USER': 'root',
+        'PASSWORD': 'r60Q!lof$',
         'HOST': 'localhost',
         'PORT': '',
     }
